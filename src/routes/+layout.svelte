9f6d121f6c74d495ebb7f622927594c4e324b186
Abid Famasya <abid.famasya@gmail.com>
2024-03-22 09:16:18 +0700

    add cf analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,3 +8,13 @@
 <QueryClientProvider client={queryClient}>
 	<slot />
 </QueryClientProvider>
+
+<svelte:head>
+	<!-- Cloudflare Web Analytics -->
+	<script
+		defer
+		src="https://static.cloudflareinsights.com/beacon.min.js"
+		data-cf-beacon="&lcub;&quot;token&quot;: &quot;fc596802ae6d4f6a906aee9203656426&quot;&rcub;"
+	></script>
+	<!-- End Cloudflare Web Analytics -->
+</svelte:head>
