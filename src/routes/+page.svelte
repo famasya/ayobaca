e8ceae4ddf9718ce4c6a12da732d6e74f5b40156
Abid Famasya <abid.famasya@gmail.com>
2024-04-03 08:41:46 +0700

    feat: add error handler

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { browser } from '$app/environment';
 	import { pushState } from '$app/navigation';
 	import { page } from '$app/stores';
+	import * as Alert from '$lib/components/ui/alert';
 	import { Badge } from '$lib/components/ui/badge';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { Card } from '$lib/components/ui/card';
@@ -10,7 +11,7 @@
 	import * as Select from '$lib/components/ui/select';
 	import { Skeleton } from '$lib/components/ui/skeleton';
 	import { createQuery } from '@tanstack/svelte-query';
-	import { ExternalLink } from 'lucide-svelte';
+	import { ExternalLink, ShieldX } from 'lucide-svelte';
 	import { writable } from 'svelte/store';
 	import type { SearchResult } from '../types';
 	import Header from './header.svelte';
@@ -102,128 +103,134 @@
 		class="h-12 border border-2 border-green-600 shadow shadow-md focus:outline-none"
 	/>
 
-	{#if searchTerm.length > 0}
-		<h4 class="mt-4 font-semibold">
-			Hasil pencarian <Badge>
-				{searchTerm}
-			</Badge>
-		</h4>
-	{/if}
-	<div class="mb-4 mt-4 grid grid-cols-3 gap-2 sm:grid-cols-4 sm:gap-4">
-		{#if ($search.isLoading || isSearching || $search.status === 'pending') && previousSearch !== searchTerm}
-			{#each Array(24) as _, index (index)}
-				<Skeleton class="h-48 rounded" />
-			{/each}
-		{:else if $search.isError}
-			<p>Error: {$search.error.message}</p>
+	{#if $search.isError}
+		<Alert.Root variant="destructive" class="my-4">
+			<ShieldX class="h-5 w-5" />
+			<Alert.Title>Terjadi kesalahan :(</Alert.Title>
+			<Alert.Description>Sepertinya terjadi kesalahan di server LetsReadAsia</Alert.Description>
+		</Alert.Root>
+	{:else}
+		{#if searchTerm.length > 0}
+			<h4 class="mt-4 font-semibold">
+				Hasil pencarian <Badge>
+					{searchTerm}
+				</Badge>
+			</h4>
 		{/if}
-		{#if $searchResults.length > 0}
-			{#each $searchResults as result}
-				{@const slug = result.name.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase()}
-				<Drawer.Root>
-					<Drawer.Trigger>
-						<Card
-							id={slug}
-							class="relative flex h-full flex-col items-center justify-center text-center shadow"
-						>
-							<img
-								id={`${slug}-cover`}
-								src={result.thumborCoverImageUrl}
-								alt={result.name}
-								height="400"
-								width="300"
-								class="h-48 object-cover"
-							/>
-							<div
-								class="absolute bottom-0 left-0 right-0 rounded-b bg-gradient-to-b from-transparent to-black/70 p-2 pt-10 font-semibold text-white"
+		<div class="mb-4 mt-4 grid grid-cols-3 gap-2 sm:grid-cols-4 sm:gap-4">
+			{#if ($search.isLoading || isSearching || $search.status === 'pending') && previousSearch !== searchTerm}
+				{#each Array(24) as _, index (index)}
+					<Skeleton class="h-48 rounded" />
+				{/each}
+			{/if}
+			{#if $searchResults.length > 0}
+				{#each $searchResults as result}
+					{@const slug = result.name.replace(/[^a-zA-Z0-9]/g, '-').toLowerCase()}
+					<Drawer.Root>
+						<Drawer.Trigger>
+							<Card
+								id={slug}
+								class="relative flex h-full flex-col items-center justify-center text-center shadow"
 							>
-								<p>{result.name}</p>
-							</div>
-						</Card>
-					</Drawer.Trigger>
-					<Drawer.Content>
-						<div class="mx-auto w-full max-w-xl">
-							<Drawer.Header>
-								<Drawer.Title>{result.name}</Drawer.Title>
-								<Drawer.Description class="mt-2 flex flex-col gap-2">
-									<p>Jumlah halaman : {result.totalPages} halaman</p>
-									<p>Tingkat kesulitan : {result.readingLevel}</p>
-									<p>
-										Kontributor :
-										{#if result.contributingUsers.length === 0}
-											Tidak tersedia
-										{:else}
-											{result.contributingUsers.join(', ')}
-										{/if}
-									</p>
-									<p>
-										Kategori : {result.tags
-											.map((t) => t.localizations[6260074016145408])
-											.join(', ')}
-									</p>
-									<p>
-										Origin URL : <a
-											href={`https://www.letsreadasia.org/book/${result.masterBookId}?bookLang=6260074016145408`}
-											target="_blank"
-											rel="noreferrer"
-											><Badge class="bg-green-700"
-												>Let's Read Asia <ExternalLink class="ml-2 h-4" /></Badge
-											></a
-										>
-									</p>
-								</Drawer.Description>
-							</Drawer.Header>
-							<div class="mx-4 mb-8 mt-4">
-								<div class="mb-8">
-									<h4 class="mb-1 font-semibold">Ringkasan</h4>
-									<p>{result.description ?? 'Tidak tersedia'}</p>
+								<img
+									id={`${slug}-cover`}
+									src={result.thumborCoverImageUrl}
+									alt={result.name}
+									height="400"
+									width="300"
+									class="h-48 object-cover"
+								/>
+								<div
+									class="absolute bottom-0 left-0 right-0 rounded-b bg-gradient-to-b from-transparent to-black/70 p-2 pt-10 font-semibold text-white"
+								>
+									<p>{result.name}</p>
 								</div>
-								<div class="flex flex-row gap-2">
-									<div class="w-1/3">
-										<Select.Root
-											selected={selectedLang}
-											onSelectedChange={(e) =>
-												e &&
-												(selectedLang = {
-													label: e.label ?? 'Bahasa Indonesia',
-													value: e.value ?? 6260074016145408
-												})}
-										>
-											<Select.Trigger class="w-full shadow">
-												<Select.Value placeholder="Pilih bahasa" />
-											</Select.Trigger>
-											<Select.Content>
-												{#each result.availableLanguages as lang}
-													<Select.Item value={lang.id} label={lang.name}>{lang.name}</Select.Item>
-												{/each}
-											</Select.Content>
-											<Select.Input name="favoriteFruit" />
-										</Select.Root>
-									</div>
-									<div class="w-2/3">
-										<div class="flex flex-row gap-2">
-											<Button
-												href="/read/{slug}/{result.masterBookId}?lang={selectedLang.value}"
+							</Card>
+						</Drawer.Trigger>
+						<Drawer.Content>
+							<div class="mx-auto w-full max-w-xl">
+								<Drawer.Header>
+									<Drawer.Title>{result.name}</Drawer.Title>
+									<Drawer.Description class="mt-2 flex flex-col gap-2">
+										<p>Jumlah halaman : {result.totalPages} halaman</p>
+										<p>Tingkat kesulitan : {result.readingLevel}</p>
+										<p>
+											Kontributor :
+											{#if result.contributingUsers.length === 0}
+												Tidak tersedia
+											{:else}
+												{result.contributingUsers.join(', ')}
+											{/if}
+										</p>
+										<p>
+											Kategori : {result.tags
+												.map((t) => t.localizations[6260074016145408])
+												.join(', ')}
+										</p>
+										<p>
+											Origin URL : <a
+												href={`https://www.letsreadasia.org/book/${result.masterBookId}?bookLang=6260074016145408`}
 												target="_blank"
-												class="w-full bg-green-700 shadow">Buka Buku</Button
+												rel="noreferrer"
+												><Badge class="bg-green-700"
+													>Let's Read Asia <ExternalLink class="ml-2 h-4" /></Badge
+												></a
 											>
+										</p>
+									</Drawer.Description>
+								</Drawer.Header>
+								<div class="mx-4 mb-8 mt-4">
+									<div class="mb-8">
+										<h4 class="mb-1 font-semibold">Ringkasan</h4>
+										<p>{result.description ?? 'Tidak tersedia'}</p>
+									</div>
+									<div class="flex flex-row gap-2">
+										<div class="w-1/3">
+											<Select.Root
+												selected={selectedLang}
+												onSelectedChange={(e) =>
+													e &&
+													(selectedLang = {
+														label: e.label ?? 'Bahasa Indonesia',
+														value: e.value ?? 6260074016145408
+													})}
+											>
+												<Select.Trigger class="w-full shadow">
+													<Select.Value placeholder="Pilih bahasa" />
+												</Select.Trigger>
+												<Select.Content>
+													{#each result.availableLanguages as lang}
+														<Select.Item value={lang.id} label={lang.name}>{lang.name}</Select.Item>
+													{/each}
+												</Select.Content>
+												<Select.Input name="favoriteFruit" />
+											</Select.Root>
+										</div>
+										<div class="w-2/3">
+											<div class="flex flex-row gap-2">
+												<Button
+													href="/read/{slug}/{result.masterBookId}?lang={selectedLang.value}"
+													target="_blank"
+													class="w-full bg-green-700 shadow">Buka Buku</Button
+												>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					</Drawer.Content>
-				</Drawer.Root>
-			{/each}
+						</Drawer.Content>
+					</Drawer.Root>
+				{/each}
+			{/if}
+		</div>
+		{#if !isSearching && $searchResults.length === 0 && $search.status !== 'pending'}
+			<p class="mb-4 rounded bg-gray-100 p-12 text-center">Tidak ada hasil</p>
 		{/if}
-	</div>
-	{#if !isSearching && $searchResults.length === 0 && $search.status !== 'pending'}
-		<p class="mb-4 rounded bg-gray-100 p-12 text-center">Tidak ada hasil</p>
+		<Button
+			class="bg-green-700 shadow hover:bg-green-800"
+			on:click={() => (cursor = $search.data?.cursorWebSafeString ?? '')}
+			disabled={$search.isLoading || $search.data?.cursorWebSafeString === null}
+			>{$search.isLoading ? 'Memuat...' : 'Selanjutnya'}</Button
+		>
 	{/if}
-	<Button
-		class="bg-green-700 shadow hover:bg-green-800"
-		on:click={() => (cursor = $search.data?.cursorWebSafeString ?? '')}
-		disabled={$search.isLoading || $search.data?.cursorWebSafeString === null}
-		>{$search.isLoading ? 'Memuat...' : 'Selanjutnya'}</Button
-	>
 </div>
